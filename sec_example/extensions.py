4c0e4afef728a58c003c293dff2e5e3634e7ef7b
yizhimanpadewoniu <yizhimanpadewoniu@yeah.net>
2019-06-17 19:28:54 +0800

    update

---
+++ sec_example/extensions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2019/4/14 13:13
+# @Author : Am4zing
+# @Site :
+# @File : __init__.py.py
+# @Software: PyCharm
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+db = SQLAlchemy()
+bootstrap = Bootstrap()
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from sec_example import create_app
+from flask import Flask
+
+app = create_app('production')
+# app = Flask('sec_example')
